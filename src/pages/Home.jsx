23eacf287fa9f1c9fb2d219c89bd9b2a54079157
Louisit0 <lrangarita7@gmail.com>
2023-07-10 16:11:22 -0300

    Contenido generico

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-9xl">Bienvenido a MartialGear</h1>
+      <p className="text-lg mt-10">
+        Descubre una amplia selección de productos de artes marciales en
+        MartialGear. Desde uniformes y equipos de entrenamiento hasta accesorios
+        y protecciones, ofrecemos todo lo que necesitas para practicar y mejorar
+        en tus disciplinas marciales favoritas. Navega por nuestras categorías,
+        encuentra los mejores productos de calidad y equípate para alcanzar tus
+        metas en las artes marciales. ¡Explora nuestro catálogo y comienza tu
+        aventura marcial hoy mismo! 🥊
+      </p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/SobreNosotros.jsx
@@ -0,0 +1,15 @@
+const SobreNosotros = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">Sobre nosotros</h1>
+      <p className="text-lg mt-10">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas cumque
+        totam assumenda saepe debitis nobis cupiditate corporis cum ad libero
+        amet iste deleniti, officia sunt? Adipisci aliquid reiciendis ipsam
+        dignissimos.
+      </p>
+    </div>
+  );
+};
+
+export default SobreNosotros;
