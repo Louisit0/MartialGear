c520becedb52d6f07f0efae2263b5f9e4ff29aa6
Louisit0 <lrangarita7@gmail.com>
2023-07-10 01:30:32 -0300

    Nuevos links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,10 @@
 const NavBar = () => {
   return (
     <nav className="flex flex-row items-end gap-10 py-10">
-      <h2 className="text-3xl">MartialGear</h2>
-      <a href="">Productos</a>
+      <h2 className="text-3xl font-semibold">MartialGear</h2>
+      <a href="">Kickboxing</a>
+      <a href="">Boxeo</a>
+      <a href="">Taekwondo</a>
       <a href="">Sobre nosotros</a>
       <a href="">FAQ</a>
     </nav>
