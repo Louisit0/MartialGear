23d0769cfca74d261400ae808080e590fe5d406d
Louisit0 <lrangarita7@gmail.com>
2023-07-27 23:57:00 -0300

    nuevo nombre

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <div>
       <nav className="flex flex-row items-end gap-10 py-10">
         <Link to="/" className="text-3xl font-semibold">
-          MartialGear
+          PCGear
         </Link>
         <Link to="/productos">Productos</Link>
         <Link to="/sobreNosotros">Sobre nosotros</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,12 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-9xl">Bienvenido a MartialGear</h1>
+      <h1 className="text-9xl">Bienvenido a PCGear</h1>
       <p className="text-lg mt-10">
-        Descubre una amplia selección de productos de artes marciales en
-        MartialGear. Desde uniformes y equipos de entrenamiento hasta accesorios
-        y protecciones, ofrecemos todo lo que necesitas para practicar y mejorar
-        en tus disciplinas marciales favoritas. Navega por nuestras categorías,
-        encuentra los mejores productos de calidad y equípate para alcanzar tus
-        metas en las artes marciales. ¡Explora nuestro catálogo y comienza tu
-        aventura marcial hoy mismo! 🥊
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga
+        perferendis nobis totam fugit minus unde similique, veritatis eos quam,
+        sint, pariatur rerum aspernatur voluptate eligendi ullam omnis
+        accusamus? Omnis, tenetur.
       </p>
     </div>
   );
