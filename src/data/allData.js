ecb9385f8b88d3cd8ec324dbde685fa5fa5f5906
Louisit0 <lrangarita7@gmail.com>
2023-08-04 17:01:28 -0300

    array all data

---
+++ src/data/allData.js
@@ -0,0 +1,63 @@
+export const dataProductos = [
+  {
+    id: 1,
+    nombre: "Logitech G Pro X Superlight Wireless",
+    precio: 85555,
+    imagen:
+      "https://www.venex.com.ar/products_images/1625151199_pro-x-superlight-black-gallery-1.png",
+    descripcion: "Mouse superligero, innalambrico, para trabajar y jugar.",
+  },
+  {
+    id: 2,
+    nombre: "Samsumg Odyssey G4",
+    precio: 159000,
+    imagen: "https://nuevatest.cl/wp-content/uploads/2023/01/1-2.svg",
+    descripcion: "Monitor 1080p, 240hz de 25 pulgadas.",
+  },
+  {
+    id: 3,
+    nombre: "HyperX Cloud II",
+    precio: 70000,
+    imagen:
+      "https://www.venex.com.ar/products_images/1534799941_1429219484957260301909173.jpg",
+    descripcion: "Headset de calidad, para música y juegos.",
+  },
+  {
+    id: 4,
+    nombre: "Logitech G Pro X Superlight Wireless",
+    precio: 85555,
+    imagen:
+      "https://www.venex.com.ar/products_images/1625151199_pro-x-superlight-black-gallery-1.png",
+    descripcion: "Mouse superligero, innalambrico, para trabajar y jugar.",
+  },
+  {
+    id: 5,
+    nombre: "Samsumg Odyssey G4",
+    precio: 159000,
+    imagen: "https://nuevatest.cl/wp-content/uploads/2023/01/1-2.svg",
+    descripcion: "Monitor 1080p, 240hz de 25 pulgadas.",
+  },
+  {
+    id: 6,
+    nombre: "HyperX Cloud II",
+    precio: 70000,
+    imagen:
+      "https://www.venex.com.ar/products_images/1534799941_1429219484957260301909173.jpg",
+    descripcion: "Headset de calidad, para música y juegos.",
+  },
+  {
+    id: 7,
+    nombre: "Logitech G Pro X Superlight Wireless",
+    precio: 85555,
+    imagen:
+      "https://www.venex.com.ar/products_images/1625151199_pro-x-superlight-black-gallery-1.png",
+    descripcion: "Mouse superligero, innalambrico, para trabajar y jugar.",
+  },
+  {
+    id: 8,
+    nombre: "Samsumg Odyssey G4",
+    precio: 159000,
+    imagen: "https://nuevatest.cl/wp-content/uploads/2023/01/1-2.svg",
+    descripcion: "Monitor 1080p, 240hz de 25 pulgadas.",
+  },
+];
