34dac6acc480e820d5642cf617fda91ee509a9c8
Louisit0 <lrangarita7@gmail.com>
2023-08-07 17:28:58 -0300

    cantidad y precio total al añadir o disminuir los productos

--- src/pages/Categorias/ProductoDetalle.jsx
+++ src/pages/Categorias/ProductoDetalle.jsx
@@ -1,20 +1,33 @@
 import { useParams } from "react-router-dom";
 import { dataProductos } from "../../data/allData";
+import { useState } from "react";
 
-const ProductoDetalle = () => {
-  // Obtener el parámetro de la URL (productosId)
+const ProductoDetalle = ({ toggleDrawer }) => {
+  const [cant, setCant] = useState(1);
   const { productosId } = useParams();
-
   const producto = dataProductos.find(
     (producto) => producto.id === parseInt(productosId)
   );
 
+  const [precioTotal, setPrecioTotal] = useState(producto.precio);
+
+  const aumentarCant = () => {
+    if (cant < producto.stock) {
+      setCant((prevCant) => prevCant + 1);
+      setPrecioTotal((prevPrecioTotal) => prevPrecioTotal + producto.precio);
+    }
+  };
+
+  const disminuirCant = () => {
+    if (cant > 1) {
+      setCant((prevCant) => prevCant - 1);
+      setPrecioTotal((prevPrecioTotal) => prevPrecioTotal - producto.precio);
+    }
+  };
+
   return (
     <div className="w-full h-full mt-32 relative">
-      <h2 className="font-bold text-2xl left-80 mb-3">
-        Detalles del producto con ID {productosId}
-      </h2>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row">
         <div className="w-1/2">
           <img
             src={producto.imagen}
@@ -23,11 +36,15 @@ const ProductoDetalle = () => {
           />
         </div>
         <div className="w-1/2 bg-gray-200 py-20 px-10">
+          <h2 className="font-bold text-3xl mb-4">{producto.nombre}</h2>
           <p>{producto.descripcion}</p>
           <div className="flex flex-row items-center justify-between mt-10">
-            <p className="font-bold text-xl">Quantity</p>
+            <p className="font-bold text-xl">{producto.stock} disponibles!</p>
             <div className="flex flex-row">
-              <button className="p-2 bg-white border border-black">
+              <button
+                className="p-2 bg-white border border-black"
+                onClick={disminuirCant}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -43,8 +60,13 @@ const ProductoDetalle = () => {
                   />
                 </svg>
               </button>
-              <p className="py-2 px-4 bg-gray-300y border border-black">1</p>
-              <button className="p-2 bg-white border border-black">
+              <p className="py-2 px-4 bg-gray-300y border border-black">
+                {cant}
+              </p>
+              <button
+                className="p-2 bg-white border border-black"
+                onClick={aumentarCant}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -61,9 +83,12 @@ const ProductoDetalle = () => {
                 </svg>
               </button>
             </div>
-            <p className="font-bold text-xl">{`$ ${producto.precio}`}</p>
+            <p className="font-bold text-xl">{`$ ${precioTotal}`}</p>
           </div>
-          <button className="p-4 bg-red-700 text-white uppercase mt-10">
+          <button
+            className="p-4 bg-red-700 text-white uppercase mt-10"
+            onClick={toggleDrawer}
+          >
             Añadir al carrito
           </button>
         </div>
