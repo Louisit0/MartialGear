d5f8ab33f044c4fad57963c67f67e1a1e9566466
Luis <lrangarita7@gmail.com>
2023-08-29 17:42:54 -0300

    some changes

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { dataProductos } from "../data/allData";
 import Tabs from "../components/Tabs";
@@ -6,21 +7,20 @@ const Productos = () => {
   return (
     <div className="flex flex-col mt-36">
       <Tabs />
-
       <div className="grid md:grid-cols-3 gap-4 mx-4 md:mx-0">
         {dataProductos.map((producto) => (
           <Link
             key={producto.id}
             to={`/productos/${producto.id}`}
-            className="cursor-pointer"
+            className="cursor-pointer bg-white border border-gray-200 shadow-sm rounded-lg"
           >
-            <div className="h-full bg-white border border-gray-200 shadow-sm rounded-lg">
+            <div className="w-full h-full">
               <img
                 src={producto.imagen}
                 alt={producto.nombre}
-                className="rounded-t-lg object-cover"
+                className="object-cover mx-auto"
               />
-              <div className="m-4 px-4">
+              <div className="flex flex-col px-4">
                 <h4 className="font-bold">{producto.nombre}</h4>
                 <p className="my-4 text-sm">{producto.descripcion}</p>
                 <p className="text-green-600">{`$ ${producto.precio}`}</p>
