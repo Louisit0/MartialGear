2d27359f24dbdfa56612d3a0e2d644b488fe480d
Luis <lrangarita7@gmail.com>
2023-08-31 16:56:34 -0300

    logo en navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,11 @@ const NavBar = ({ notificationCount }) => {
     <div>
       <nav className="flex flex-row fixed top-0 bg-white z-50 w-full justify-around py-10">
         <Link to="/" className="text-3xl font-semibold">
-          PCGear
+          <img
+            src="https://www.madkat.store/images/logo-black.png"
+            alt="logo"
+            width={50}
+          />
         </Link>
         <div className="flex gap-4 text-sm md:gap-8 md:text-base items-center">
           <Link to="/productos">Productos</Link>
