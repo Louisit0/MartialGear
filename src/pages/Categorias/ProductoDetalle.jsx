b39c19d130e83d41717478b66dd6281bf7b8e4ca
Luis <lrangarita7@gmail.com>
2023-09-01 15:17:51 -0300

    remove sumar y restar

--- src/pages/Categorias/ProductoDetalle.jsx
+++ src/pages/Categorias/ProductoDetalle.jsx
@@ -74,65 +74,41 @@ const ProductoDetalle = ({
           <img
             src={producto.imagen}
             alt={producto.nombre}
-            className="object-cover h-full"
+            className="object-cover w-2/3 mx-auto md:h-full md:w-full"
           />
         </div>
-        <div className="w-full md:w-1/2 bg-gray-200 py-20 px-10">
-          <h2 className="font-bold text-3xl mb-4">{producto.nombre}</h2>
-          <p>{producto.descripcion}</p>
-          <div className="flex flex-col md:flex-row items-center justify-between mt-10">
-            <p className="font-bold text-xl">Cantidad:</p>
-            <div className="flex flex-row">
-              <button
-                className="p-2 bg-white border border-black"
-                onClick={disminuirCant}
-              >
+        <div className="w-full md:w-1/2 bg-gray-50 py-20 px-10">
+          <div className="flex flex-col justify-center h-full">
+            <p className="text-gray-400">{producto.categoria}</p>
+            <h2 className="font-bold text-3xl mb-4">{producto.nombre}</h2>
+            <p>{producto.descripcion}</p>
+            <div className="flex flex-row justify-between w-4/5 md:w-1/2 mt-4">
+              <div className="flex flex-row">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
                   viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
+                  fill="currentColor"
+                  className="w-5 h-5 text-yellow-400 mr-2"
                 >
                   <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M18 12H6"
+                    fillRule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clipRule="evenodd"
                   />
                 </svg>
-              </button>
-              <p className="py-2 px-4 bg-gray-300y border border-black">
-                {cant}
-              </p>
-              <button
-                className="p-2 bg-white border border-black"
-                onClick={aumentarCant}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6v12m6-6H6"
-                  />
-                </svg>
-              </button>
+                <p className="text-gray-400">{producto.puntuacion}</p>
+              </div>
+              <p>-</p>
+
+              <p className="font-bold">${producto.precio}</p>
             </div>
-            <p className="font-bold text-xl">{`$ ${precioTotal}`}</p>
+            <button
+              className="p-4 bg-black text-white uppercase font-bold mt-10 md:w-1/2"
+              onClick={handleButtonClick}
+            >
+              Añadir al carrito
+            </button>
           </div>
-          <button
-            className="p-4 bg-red-700 text-white uppercase mt-10"
-            onClick={handleButtonClick}
-          >
-            Añadir al carrito
-          </button>
         </div>
       </div>
     </div>
