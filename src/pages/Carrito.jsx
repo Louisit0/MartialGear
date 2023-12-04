e3f76158699d606f94e4c59eb9d64b39c91bce14
Louisit0 <lrangarita7@gmail.com>
2023-12-04 00:39:29 -0300

    Update NavBar and Carrito components

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -93,11 +93,25 @@ const Carrito = ({ carritoItems, setCarritoItems }) => {
             <h2 className="text-3xl font-bold w-2/3 text-center mt-8">
               Tu carrito parece vacío
             </h2>
-            <p className="mt-4 mb-8 text-sm">Agregue artículos a su carrito.</p>
+            <p className="my-6">Agregue artículos a su carrito.</p>
             <Link
               to="/productos"
-              className="p-4 bg-black text-white w-2/3 font-bold text-center"
+              className="p-4 bg-black text-white w-2/3 font-bold rounded-xl text-center text-lg flex flex-row gap-2 items-center justify-center"
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                />
+              </svg>
               Seguir comprando
             </Link>
           </div>
